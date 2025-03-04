05e9df71cec16fa0dca4698e13e93a648faf1b54
Zakarum <zaq.dev@icloud.com>
2025-03-04 11:44:11 +0100

    Fix color pickers

--- src/color.rs
+++ src/color.rs
@@ -80,19 +80,61 @@ impl EguiProbe for EguiProbeRgba<'_, Rgba> {
         egui::color_picker::color_edit_button_rgba(
             ui,
             self.value,
-            egui::color_picker::Alpha::Opaque,
+            egui::color_picker::Alpha::BlendOrAdditive,
         )
     }
 }
 
-impl EguiProbe for EguiProbeRgbaUnmultiplied<'_, Rgba> {
+impl EguiProbe for EguiProbeRgbaPremultiplied<'_, Rgba> {
     #[inline(always)]
     fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
         egui::color_picker::color_edit_button_rgba(
             ui,
             self.value,
-            egui::color_picker::Alpha::Opaque,
+            egui::color_picker::Alpha::BlendOrAdditive,
         )
+    }
+}
+
+impl EguiProbe for EguiProbeRgb<'_, [u8; 3]> {
+    #[inline(always)]
+    fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
+        ui.color_edit_button_srgb(self.value)
+    }
+}
+
+impl EguiProbe for EguiProbeRgbaPremultiplied<'_, [u8; 4]> {
+    #[inline(always)]
+    fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
+        ui.color_edit_button_srgba_premultiplied(self.value)
+    }
+}
+
+impl EguiProbe for EguiProbeRgbaUnmultiplied<'_, [u8; 4]> {
+    #[inline(always)]
+    fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
+        ui.color_edit_button_srgba_unmultiplied(self.value)
+    }
+}
+
+impl EguiProbe for EguiProbeRgb<'_, [f32; 3]> {
+    #[inline(always)]
+    fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
+        ui.color_edit_button_rgb(self.value)
+    }
+}
+
+impl EguiProbe for EguiProbeRgbaPremultiplied<'_, [f32; 4]> {
+    #[inline(always)]
+    fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
+        ui.color_edit_button_rgba_premultiplied(self.value)
+    }
+}
+
+impl EguiProbe for EguiProbeRgbaUnmultiplied<'_, [f32; 4]> {
+    #[inline(always)]
+    fn probe(&mut self, ui: &mut egui::Ui, _style: &crate::Style) -> egui::Response {
+        ui.color_edit_button_rgba_unmultiplied(self.value)
     }
 }
 
